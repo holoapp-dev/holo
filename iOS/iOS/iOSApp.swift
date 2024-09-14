c28bf76ea0edeb0f9e7a8b57ab4e76b7fd392e8b
Nurikan <81532722+Nurikan-sketch@users.noreply.github.com>
2024-09-14 21:11:14 +0200

    Add files via upload

---
+++ iOS/iOS/iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSApp.swift
+//  iOS
+//
+//  Created by Yan on 14.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOS/iOSTests/iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOSTests.swift
+//  iOSTests
+//
+//  Created by Yan on 14.09.2024.
+//
+
+import Testing
+@testable import iOS
+
+struct iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
