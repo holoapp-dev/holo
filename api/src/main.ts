015d301c1556425c0d3ee3dee0bc1c475b9c75ff
Kirill Belolipetsky <archhaze24@gmail.com>
2024-10-08 13:26:23 +0300

    use dotenvx instead of dotenv

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,10 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
-
-dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
